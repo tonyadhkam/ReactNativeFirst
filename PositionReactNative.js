2c48c2bf33feb711521688f9759fec948d47e02b
tonyadhkam <tonyandhika20@gmail.com>
2021-12-20 15:08:32 +0700

    update

--- PositionReactNative.js
+++ PositionReactNative.js
@@ -17,9 +17,40 @@ const PositionReactNative = () => {
 export default PositionReactNative;
 
 const styles = StyleSheet.create({
-    wrapper: {padding: 20, alignItems: 'center'},
-    cartWrapper: {borderWidth: 1, borderColor: 'red', width: 93, height: 93, borderRadius: 93/2, alignItems: 'center', justifyContent: 'center', position: 'relative', marginTop: 40},
-    iconCart: {width: 50, height: 50},
-    text: {fontSize: 12, color: '#777777', fontWeight: '700', marginTop: 8},
-    notif: {fontSize: 12, color: 'white', backgroundColor: '#6FCF97', padding: 4, borderRadius: 25, width: 24, height: 24, position: 'absolute', top: 0, right: 0}
+    wrapper: {
+        padding: 20, 
+        alignItems: 'center'},
+
+    cartWrapper: {
+        borderWidth: 1, 
+        borderColor: 'red', 
+        width: 93, 
+        height: 93, 
+        borderRadius: 93/2, 
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        position: 'relative', 
+        marginTop: 40},
+
+    iconCart: {
+        width: 50, 
+        height: 50},
+
+    text: {
+        fontSize: 12, 
+        color: '#777777', 
+        fontWeight: '700', 
+        marginTop: 8},
+
+    notif: {
+        fontSize: 12, 
+        color: 'white', 
+        backgroundColor: '#6FCF97', 
+        padding: 4, 
+        borderRadius: 25, 
+        width: 24, 
+        height: 24, 
+        position: 'absolute', 
+        top: 0, 
+        right: 0}
 })
